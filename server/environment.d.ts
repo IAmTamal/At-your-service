572ba9e4096a493dbbe94c743074ab1d8bf5f531
Tamal <gyansujan69@gmail.com>
2023-03-31 21:28:35 +0530

    feat: added server files

--- server/environment.d.ts
+++ server/environment.d.ts
@@ -6,6 +6,7 @@ declare global {
         PORT?: string;
         MONGO_DB_URI: string;
         PWD: string;
+        JWT_SECRET: string;
       }
     }
   }
--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import connectToMongo from "./database/db";
 import express from "express";
+import cookieParser from 'cookie-parser';
 
 var cors = require("cors");
 import dotenv from "dotenv";
@@ -12,10 +13,14 @@ let port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("HELLO FROM HOME");
 });
+
+//auth routes
+app.use("/auth", require("./routes/Auth"));
 
 
 app.listen(port, () => console.log("API IS RUNNING 🚀"));
