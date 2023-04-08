0b235d77ce1b4ec78d97b3eddfbfda21126eefde
Tamal <gyansujan69@gmail.com>
2023-04-08 22:11:05 +0530

    fix: mapping proper data

--- src/interface/interfaces.ts
+++ src/interface/interfaces.ts
@@ -5,3 +5,20 @@ export interface ICredentials {
     email: string;
     password: string;
 }
+
+export interface Profiledataprops {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    userName: string;
+    email: string;
+    password: string;
+    servicesprovided: string[];
+    charge: number;
+    keywords: string[];
+    slug: string;
+    profilepic: string;
+    availability: string[];
+    createdAt: string;
+    __v: number;
+}
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -8,12 +8,11 @@ import Testimonials from './testimonials/Testimonials';
 
 
 
-
 const Profile = () => {
 
     const params = useParams();
     const { data: profiledata, error } = useSWR('profile-data', fetchProfiledata);
-    console.log(profiledata);
+
 
 
     return (
@@ -22,18 +21,18 @@ const Profile = () => {
             <div className="profile_parent">
 
                 <div className="profile_hero">
-                    {/* <img src={profiledata?.profilepic} alt="" /> */}
+                    {/* <img src={profiledata?.data.profilepic} alt="" /> */}
                     <img src="https://pbs.twimg.com/profile_images/1639235422932074499/qiGUX-3O_400x400.png" alt="" />
 
                     <div className="hero_contentdiv">
-                        <h1>{profiledata?.firstName + " " + profiledata?.lastName}</h1>
+                        <h1>{profiledata?.data.firstName + " " + profiledata?.data.lastName}</h1>
                         <p>Need professional, yet affordable cleaning services to clean up your yard ? <br /> Hire me !</p>
 
                         <button className='btn'>Drop a mail</button>
 
-                        <div className="socialsdiv">
-                            <a href={profiledata?.facebook} target="_blank" rel="noreferrer"><i className="fab fa-facebook-f"></i></a>
-                        </div>
+                        {/*  <div className="socialsdiv">
+                            <a href={profiledata?.data.facebook} target="_blank" rel="noreferrer"><i className="fab fa-facebook-f"></i></a>
+                        </div> */}
                     </div>
                 </div>
 
